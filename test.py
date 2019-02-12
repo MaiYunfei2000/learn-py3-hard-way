9107a7b3b0ff72b6ef640a2f7ba7823af003c594
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-02-12 19:09:10 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-print(round(8 / 4))
-print("Hi")
-print("What happened?")
-print("连" + "起来")
-# 那如果去掉加号，甚至去掉空格呢？
-print("连""起来")
-# 这效果不是一样嘛。
+print("{}".format("snow"))
 
-.format("snow")
+x = """hhhhh
+hhhhh
+hhhh
+hhhhh"""
 
-print("{}")
+print(x)
