4c4cb3b410bbe2cb055a1c8baed109c5ec426fdc
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-02-12 19:30:21 +0800

    Create ex8.py

---
+++ ex8.py
@@ -0,0 +1,26 @@
+formatter = "{} {} {} {}"
+
+# 取代码 formatter = "{} {} {} {}" 定义的字符串，并给format依次传递4个参数。
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+# 嵌套中的嵌套！
+print(formatter.format(
+formatter, formatter, formatter, 
+# 就算让参变量跳行，也不会跳行。
+formatter))
+# 字符串同理。
+print(formatter.format(
+    "Try you",
+    "Own text here",
+    "Maybe a poem",
+    "Or a song about fear"
+    "我才不_(:з」∠)_"
+))
+
+# MYF的自由发挥时间↓
+print()
+print("那么我还可以试试：")
+x = 5
+x = x * x
+print(formatter.format(formatter.format(1, 2, 3, 4), formatter, 3, x))
