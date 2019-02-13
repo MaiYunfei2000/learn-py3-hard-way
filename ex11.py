9b14cbe0959c5b8a1bae4ad6f305163be02e030e
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-02-13 17:58:01 +0800

    Add comment

--- ex11.py
+++ ex11.py
@@ -1,4 +1,8 @@
+# 此练习中新学了函数：input()。
+
+# 打印“How old are you?”，不跳行，用空格连上下一行的内容。
 print("How old are you?", end=' ')
+# 让用户输入一个值，并将这个值赋给变量age。
 age = input()
 print("How tall are you?", end=' ')
 height = input()
