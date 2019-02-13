c75bf121fbdc9a090f26d485f089eeae01e697a6
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-02-13 18:35:14 +0800

    Update ex12.py

--- ex12.py
+++ ex12.py
@@ -1,11 +1,13 @@
 # 此练习巩固ex11所学。并新学了在input函数的括号里键入字符串来达成提示别人应输入什么东西的效果。
-print("How old are you?", input())
+
+print("input numbers only, or it will error. \n")
 # 效果等同于：
 # print("How old are you?", end=' ')
 # age = input()
 # 也就是用一行代码实现了两行代码的功能。
 age = input("How old are you? ")
-height = input("How tall are you? ")
-weight = input("How much do you weigh? ")
+# 提示用户输入身高并取整，然后赋值给变量height。
+height = int(input("How tall are you? (cm) "))
+weight = input("How much do you weigh? (kg) ")
 
-print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+print(f"So, you're {age} old, {height} cm tall and {weight} kg heavy.")
