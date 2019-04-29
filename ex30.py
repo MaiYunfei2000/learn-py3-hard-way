f0bf3fbea66b089880fdf2b43c1e353f530faa26
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-04-29 12:01:12 +0800

    Add comments

--- ex30.py
+++ ex30.py
@@ -1,12 +1,16 @@
+# 此练习中学习了if、elif（应该是else if的意思吧）、else。在这里else与另外两者的关系应该是 else为(if和elif)的否定，也就是if和elif条件的交集的补集。
+
 people = 30
 cars = 40
 trucks = 15
 
-
+# 如果cars大于people
 if cars > people:
     print("We should take the cars.")
+# 又或者如果cars小于people
 elif cars < people:
     print("We should not take the cars.")
+# 如果cars既不大于也不小于people，也就是cars等于people
 else:
     print("We can't decide.")
 
