942d02f43117f4c34111d60eecd66541314aaea7
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-05-04 10:37:02 +0800

    Add comments

--- ex33.py
+++ ex33.py
@@ -1,8 +1,12 @@
+# i的初始值为0
 i = 0
+# 建立空表格numbers
 numbers = []
 
+# 当i小于6时，进行以下循环：
 while i < 6:
     print(f"At the top i is {i}")
+    # 将当前的i值添加至列表末尾
     numbers.append(i)
     
     i = i + 1
@@ -12,5 +16,6 @@ while i < 6:
 
 print("The numbers: ")
 
+# 将表格numbers中的元素一个个赋给num，一次次执行此语句
 for num in numbers:
     print(num)
