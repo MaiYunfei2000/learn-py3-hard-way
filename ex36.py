e17c1c414a21c106690bf92c2fe3add3a733aa7c
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-05-09 15:46:01 +0800

    Create ex36.py

---
+++ ex36.py
@@ -0,0 +1,13 @@
+import random
+
+# if不一定要有一个else跟着
+
+# 例如：
+n = float(input("input a number: "))
+if n < 5:
+    print("aaa... ok.")
+# 如果不符合if就直接过了
+
+while n < 5:
+    print(random.randrange(100))
+# NOoooooooooo!
