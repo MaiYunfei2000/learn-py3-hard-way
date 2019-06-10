ed4f3443466295850cd9d4cdc2698a30305fb423
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-06-10 16:00:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
-def isInt(num):
-    # try和except是什么作用？
-    try:
-        num = int(str(num))
-        return isinstance(num, int)
-    except:
-        return False
-
-choice = (isInt(input("> ")))
-
-print(choice)
+n = input()
+print(n != 0)
