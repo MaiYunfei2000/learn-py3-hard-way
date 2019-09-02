b16c4af24f48f13b299d66a00871514cd68d5fed
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-09-02 16:02:47 +0800

    Update ex40a.py

--- ex40a.py
+++ ex40a.py
@@ -15,8 +15,24 @@ print(ex40mystuff.tangerine)
 
 print("\n比较一下字典和模块：\n")
 
-ex40mystuff['apple'] # get apple from dict
+print(mystuff['apple']) # get apple from dict
 ex40mystuff.apple() # get apple from the module
-ex40mystuff.tangerine # same thing, it's just a variable
+print(ex40mystuff.tangerine) # same thing, it's just a variable
 
 # 吐槽：其实就是可以调用外部文件的代码，然后取了个“模块”的名字——叫着方便？= =
+
+print("\n下面来认识一下“类”(class)\n")
+
+class MyStuff(object):
+    
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASSY APPLES!")
+
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
+
+# 搞不懂了吧，那么继续进入正式练习环节。
