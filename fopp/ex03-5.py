63f527d0c0d500eeb800b372783bedb7340f7edc
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-09-28 13:47:39 +0800

    Update ex03-5.py

--- fopp/ex03-5.py
+++ fopp/ex03-5.py
@@ -1,3 +1,5 @@
+# many kinds of practice about "string"
+
 # 3.5.1
 
 name = "Python语言程序设计"
@@ -16,7 +18,7 @@ print(name[0], name[7], name[11], name[2:-4], name[:6], name[6:], name[:], sep='
 
 # break: 不如从https://docs.python.org/zh-cn/3/library/stdtypes.html?highlight=str#text-sequence-type-str或https://docs.python.org/3/library/stdtypes.html?highlight=str#text-sequence-type-str开始，联系完所有的代码，然后再回课本康康
 
-print('\nstr.capitalize()\n')
+print('\n★ str.capitalize()\n')
 # Return a copy of the string with its first character capitalized and the rest lowercased.
 
 a = 'apple'
@@ -24,21 +26,21 @@ print(a.capitalize())
 print('apple'.capitalize())
 # print(capitalize(a)) is wrong usage
 
-print('\nstr.casefold()\n')
+print('\n★ str.casefold()\n')
 # Return a casefolded copy of the string.
 
 b = 'China'
 c = 'ΑΒΓΔ'
 print(b.casefold(), c.casefold())
 
-print('\nstr.center(width[, fillchar])\n')
+print('\n★ str.center(width[, fillchar])\n')
 # Return centered in a string of length width. Padding is done using the specified fillchar (default is an ASCII space). The original string is returned if width is less than or equal to len(s).
 
 test = 'test aaa bbb ccc'
 print(test.center(15,"c"))
 # 🚧怎么捣鼓都没高明白干啥用的……
 
-print('\nstr.count(sub[, start[, end]])\n')
+print('\n★ str.count(sub[, start[, end]])\n')
 # Return the number of non-overlapping occurrences of substring sub in the range [start, end]. Optional arguments start and end are interpreted as in slice notation.
 
 t = 't'
@@ -48,13 +50,13 @@ print(test.count('test', 0, 4))
 print(test.count('test', 4))
 # 哦，要有end的话前面必须有start
 
-print('\nstr.encode(encoding="utf-8", errors="strict")\n')
+print('\n★ str.encode(encoding="utf-8", errors="strict")\n')
 
 print(test.encode())# 输出了“b'test aaa bbb ccc'”是什么意思？
 print('b'.encode())
 # 🚧没搞懂
 
-print('\nstr.endswith(suffix[, start[, end]])\n')
+print('\n★ str.endswith(suffix[, start[, end]])\n')
 # Return True if the string ends with the specified suffix, otherwise return False. suffix can also be a tuple of suffixes to look for. With optional start, test beginning at that position. With optional end, stop comparing at that position.
 print(test.endswith('c'))
 print(test.endswith('ccc'))
@@ -62,7 +64,7 @@ print(test.endswith('ccb'))
 print(test.endswith('t', 4))
 print(test.endswith('t', 0, 4))
 
-print('\nstr.expandtabs(tabsize=8)\n')
+print('\n★ str.expandtabs(tabsize=8)\n')
 
 d = 'test\ttesttest\ttesttesttest\t'
 e = 'test\rtesttest\rtesttesttest\r'
@@ -81,7 +83,7 @@ test            testtest        testtesttest
 '''
 # 🚧没搞懂
 
-print('\nstr.find(sub[, start[, end]])\n')
+print('\n★ str.find(sub[, start[, end]])\n')
 # Return the lowest index in the string where substring sub is found within the slice s[start:end]. Optional arguments start and end are interpreted as in slice notation. Return -1 if sub is not found.
 
 print(test.find('ccc')) # 返回的是13（而第一个c是第14个字符）
@@ -89,7 +91,7 @@ print(test.find('c', 13)) # 返回13
 print(test.find('c', 14)) # 返回14
 print(test.find('test')) # 返回0即首位
 
-print('\nstr.format(*args, **kwargs)\n')
+print('\n★ str.format(*args, **kwargs)\n')
 # Perform a string formatting operation. The string on which this method is called can contain literal text or replacement fields delimited by braces {}. Each replacement field contains either the numeric index of a positional argument, or the name of a keyword argument. Returns a copy of the string where each replacement field is replaced with the string value of the corresponding argument.
 
 a = 1
@@ -97,13 +99,55 @@ b = 2
 print('The sum of 1 + 2 is {}'.format(a+b))
 print('The sum of {} + {} is {}'.format(a, b, a+b))
 
-print('\nstr.format_map(mapping)\n')
+print('\n★ str.format_map(mapping)\n')
 # Similar to str.format(**mapping), except that mapping is used directly and not copied to a dict. This is useful if for example mapping is a dict subclass:
 
 class Default(dict):
     def __missing__(self, key):
         return key
 
-print('{name} was born in {country}'.format_map(Default_map(name='Guido')
+print('{name} was born in {country}'.format_map(Default(name='Guido')))
 
-# next https://docs.python.org/3/library/stdtypes.html?highlight=str#str.index
+# 🚧没搞懂
+
+print('\n★ str.index(sub[, start[, end]])\n')
+# Like find(), but raise ValueError when the substring is not found.
+
+print(test.find('test'))
+print(test.index('test'))
+print(test.find('tet'))
+# print(test.index('tet'))
+
+print('\n★ str.isalnum()\n')
+# Return true if all characters in the string are alphanumeric and there is at least one character, false otherwise. A character c is alphanumeric if one of the following returns True: c.isalpha(), c.isdecimal(), c.isdigit(), or c.isnumeric().
+
+print('maiyunfei'.isalnum())
+print('2000'.isalnum())
+print('maiyunfei2000'.isalnum())
+print('maiyunfei008!'.isalnum())
+print(''.isalnum())
+
+print('\n★ isalpha()\n')
+# Return true if all characters in the string are alphabetic and there is at least one character, false otherwise. Alphabetic characters are those characters defined in the Unicode character database as “Letter”, i.e., those with general category property being one of “Lm”, “Lt”, “Lu”, “Ll”, or “Lo”. Note that this is different from the “Alphabetic” property defined in the Unicode Standard.
+
+print('maiyunfei'.isalpha())
+print('2000'.isalpha())
+print('maiyunfei2000'.isalpha())
+print('maiyunfei008!'.isalpha())
+print(''.isalpha())
+
+print('\n★ str.isascii()\n')
+# Return true if the string is empty or all characters in the string are ASCII, false otherwise. ASCII characters have code points in the range U+0000-U+007F.
+
+# print('maiyunfei'.isascii()) 
+# print('2000'.isascii())
+# print('maiyunfei2000'.isascii())
+# print('maiyunfei008!'.isascii())
+# print('★'.isascii())
+# print('✨'.isascii())
+# print(''.isascii())
+# AttributeError: 'str' object has no attribute 'isascii'
+
+# 没搞懂🚧
+
+# next https://docs.python.org/3/library/stdtypes.html?highlight=str#str.isdecimal
