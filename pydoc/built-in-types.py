15b153c05aadcffa54b28c56e4e73b00ba9e1641
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-10-23 16:58:40 +0800

    Update built-in-types.py

--- pydoc/built-in-types.py
+++ pydoc/built-in-types.py
@@ -456,4 +456,15 @@ print(' '.upper().isupper())
 print('maiyunfei***'.upper())
 print('maiyunfei***'.upper().isupper())
 
-# next: https://docs.python.org/3/library/stdtypes.html?highlight=str#str.zfill
+print('\nstr.zfill(width)\n')
+# Return a copy of the string left filled with ASCII '0' digits to make a string of length width. A leading sign prefix ('+'/'-') is handled by inserting the padding after the sign character rather than before. The original string is returned if width is less than or equal to len(s).
+# For example:
+'''
+>>> "42".zfill(5)
+'00042'
+>>> "-42".zfill(5)
+'-0042'
+'''
+
+print('abc'.zfill(5))
+print('   '.zfill(5))
