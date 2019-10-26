cce3f695637417bea5711b6c399906a66cad4ed0
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-10-26 17:33:48 +0800

    Create b252.py

---
+++ pynn/b252.py
@@ -0,0 +1,132 @@
+# 2.5.2 测试网络
+
+import numpy, scipy.special, matplotlib.pyplot
+
+class neuralNetwork:
+    
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+    
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        self.lr = learningrate
+
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+        
+        self.activation_function = lambda x: scipy.special.expit(x)
+        
+        pass
+    
+    def train(self, inputs_list, targets_list):
+        
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        # the same as targets list
+        targets = numpy.array(targets_list, ndmin=2).T
+        
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+        
+        # calculate the signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        # output layer error is the (target - actual)
+        output_errors = targets - final_outputs
+        # hidden layer error is the output_errors, split by weights, recombined at hidden nodes
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+ 
+        # update the weights fo rthe links between the hidden and output layers
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(hidden_outputs))
+        
+        # update the weights for the links between the input and hidden layers
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))
+        
+        pass
+        
+    def query(self, inputs_list):
+        
+        inputs = numpy.array(inputs_list, ndmin=2).T
+
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+        
+        return final_outputs
+
+        pass
+
+input_nodes = 784
+hidden_nodes = 100
+output_nodes = 10
+
+learning_rate = 0.5
+
+# create an instance of neural network
+n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+
+# load the mnist training data CSV file into a list
+training_data_file = open("makeyourownneuralnetwork-master/mnist_dataset/mnist_train_100.csv", 'r')
+training_data_list = training_data_file.readlines()
+training_data_file.close()
+
+# train the neural network
+
+# go through all records in the training data set
+
+for record in training_data_list:
+    # split the record by the ',' commas
+    all_values = record.split(',')
+    # scale and shift the inputs
+    inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+    # create the target output values (all 0.01, except the desired label which is 0.99)
+    targets = numpy.zeros(output_nodes) + 0.01
+    # all_values[0] is the target label for this record
+    targets[int(all_values[0])] = 0.99
+    n.train(inputs, targets)
+    
+    pass
+
+# load the mnist test data CSV file into a list
+
+test_data_file = open("makeyourownneuralnetwork-master/mnist_dataset/mnist_train_100.csv", 'r')
+test_data_list = test_data_file.readlines()
+test_data_file.close()
+
+# test the neural network
+
+# scoreboard for how well the records in the test data set
+scoreboard = []
+
+# go through all the records in the test data set
+for record in test_data_list:
+    # split the record by the ',' commas
+    all_values = record.split(',')
+    # correct answer is first value
+    correct_label = int(all_values[0])
+    print(correct_label, "correct label")
+    # scale and shift the inputs
+    inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+    # query the network
+    outputs = n.query(inputs)
+    # the index of the highest value corresponds to the label
+    label = numpy.argmax(outputs)
+    print(label, "network's answer")
+    # append correct or incorrect to list
+    if (label == correct_label):
+        # network's answer matches correct answer, add 1 to scoreboard
+        scoreboard.append(1)
+    else:
+        # network's answer doesn't match correct answer, add 0 to scoreboard
+        scoreboard.append(0)
+        
+        pass
+    
+    pass
