05b0bb586497c0d7f48a58ec5bc1682c8dabde99
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-10-28 16:33:02 +0800

    Create sequence-types.py

---
+++ pydoc/std-lib/sequence-types.py
@@ -0,0 +1,10 @@
+##### Sequence Types -- list, tuple, range
+##### 序列类型：列表，元组，range对象
+
+##### https://docs.python.org/3/library/stdtypes.html#sequence-types-list-tuple-range
+
+#### common sequence operations(https://docs.python.org/3/library/stdtypes.html#common-sequence-operations)
+
+# This table lists the sequence operations sorted in ascending priority. In the table, s and t are sequences of the same type, n, i, j and k are integers and x is an arbitrary object that meets any type and value restrictions imposed by s.
+
+# The in and not in operations have the same priorities as the comparison operations. The + (concatenation) and * (repetition) operations have the same priority as the corresponding numeric operations. 
