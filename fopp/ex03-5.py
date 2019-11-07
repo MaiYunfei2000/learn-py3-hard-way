218282af0a9478c1741d261d781d043a7fbb5f81
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-11-07 14:05:57 +0800

    Update ex03-5.py

--- fopp/ex03-5.py
+++ fopp/ex03-5.py
@@ -25,3 +25,14 @@ name[:], sep='\n')
 
 # break: 不如从https://docs.python.org/zh-cn/3/library/stdtypes.html?highlight=str#text-sequence-type-str或https://docs.python.org/3/library/stdtypes.html?highlight=str#text-sequence-type-str开始，联系完所有的代码，然后再回课本康康
 # 此部分的代码已转移至pydoc/built-in-types.py
+
+x = 'he'
+y = 'llo'
+print(x + y) # 连接两个字符串
+print(x * 3) # 复制n次字符串
+print('h' in x) # 前者是否是后者的子串
+print(y[2]) # 返回第i个字符
+print('abcdefghiljklmn'[2:4]) # 返回前闭后开区间索引的子串
+
+# 返回新字符串，由元素为字符串的组合数据类型（iterable）的元素拼接而成，元素之间由str分割
+print('haha'.join(['a','b','c','fff','ggg']))
