7f46c69ff590ff87b398d7f43ccb822cfd2918b2
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2019-12-02 16:34:10 +0800

    Update sequence-types.py

--- pydoc/std-lib/built-in-types/sequence-types.py
+++ pydoc/std-lib/built-in-types/sequence-types.py
@@ -245,5 +245,18 @@ print(example5)
 #### Tuple (元组)
 # https://docs.python.org/3/library/stdtypes.html#tuples
 
+"""
+class tuple([iterable])
+
+Tuples may be constructed in a number of ways:
+
+- Using a pair of parentheses to denote the empty tuple: ()
+- Using a trailing comma for a singleton tuple: a, or (a,)
+- Separating items with commas: a, b, c or (a, b, c)
+- Using the tuple() built-in: tuple() or tuple(iterable)
+"""
+
+# 最大的区别就是元组里的元素自元组生成后不可改变
+
 #### Range
 # https://docs.python.org/3/library/stdtypes.html#ranges
