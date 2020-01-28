2f95efa0912296e7787fa6815668f797e3074c59
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2020-01-28 21:43:58 +0800

    Create fe0210.py

---
+++ mitx-6001x/fe0210.py
@@ -0,0 +1,12 @@
+# test 2
+print('\n', 'test 2', '\n')
+
+for iteration in range(5):
+    count = 0
+    while True:
+        for letter in "hello, world":
+            count += 1
+        print("Iteration " + str(iteration) + "; count is: " + str(count))
+        break
+
+# test 3
