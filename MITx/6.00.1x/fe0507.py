c3c357c0c9926279a7a3faea2a38c01034c669ac
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2020-02-13 15:16:34 +0800

    Create fe0507.py

---
+++ MITx/6.00.1x/fe0507.py
@@ -0,0 +1,21 @@
+# Here is a different piece of code for working with lists:
+
+def applyEachTo(L, x):
+    result = []
+    for i in range(len(L)):
+        result.append(L[i](x))
+    return result
+
+# Suppose that you are given the following functions:
+
+def square(a):
+    return a*a
+
+def halve(a):
+    return a/2
+
+def inc(a):
+    return a+1
+
+# For each of the following questions, indicate what value is returned. If you believe that an error will occur, write the word 'error'.
+
