115f857cc9855d0f4ffe708b73845ad0707b720d
MaiYunfei2000 <40352237+MaiYunfei2000@users.noreply.github.com>
2020-05-21 20:45:19 +0800

    Create midp7.py

---
+++ MITx/6.00.2x/midp7.py
@@ -0,0 +1,29 @@
+def solveit(test):
+    """ test, a function that takes an int parameter and returns a Boolean
+        Assumes there exists an int, x, such that test(x) is True
+        Returns an int, x, with the smallest absolute value such that test(x) is True 
+        In case of ties, return any one of them. 
+    """
+    x = 0
+    while True:
+        if x >= 1000:
+            return x
+        if test(x) == True:
+            return x
+        elif test(-x) == True:
+            return -x
+        x += 1
+
+#### This test case prints 49 ####
+def f(x):
+    return (x+15)**0.5 + x**0.5 == 15
+print(solveit(f))
+
+#### This test case prints 0 ####
+def f(x):
+    return x == 0
+print(solveit(f))
+
+def f(x):
+    return x == -4
+print(solveit(f))
